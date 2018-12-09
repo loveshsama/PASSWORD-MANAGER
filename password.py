1b53699305893ac5069980f6b3df341d64944113
loveshsama <loveshsama@gmail.com>
2018-12-09 16:02:35 +0530

    added encryption

--- password.py
+++ password.py
@@ -1,28 +1,32 @@
 # Password manager(write now just a prototype)
 # ***TO DO
 # ******** 1)Add a file to store data maybe jscon or xml or cvs to store passwords-->//done
-#*** 2)use encryption to make it secure
-#*** 3) decryption on copying
-#*** 4) use some easy algo
+#*** 2)use encryption to make it secure//done
+#*** 3) decryption on copying//done
+#*** 4) use some easy algo//done
 
 import sys, pyperclip,csv
+from crypt import Cryption
 
 #checking if command line argument is supplied or not
-if len(sys.argv) < 3:
-    print('Usage python password.py [firstname] [account] -- copy account password for a user')
+if len(sys.argv) < 4:
+    print('Usage python password.py [firstname] [account] [key] -- copy account password for a user')
     sys.exit()
 
 #inputing account name && firstname
 firstname = sys.argv[1]
 account = sys.argv[2]
+key = int(sys.argv[3])
+a = Cryption()
 
 #opening file
 with open('password.csv') as mypass:
     pass_reader = csv.DictReader(mypass)
 
     for mypassword in pass_reader:
-        if(mypassword['account']==account) and (mypassword['firstname']==firstname):
-            pyperclip.copy(mypassword['password'])
+        if(mypassword['account'].lower()==account.lower()) and (mypassword['firstname'].lower()==firstname.lower()):
+            #pyperclip.copy(mypassword['password'])
+            pyperclip.copy(a.decrytion(mypassword['password'],key))
             print('password copied to clipboard')
             sys.exit()
 
@@ -33,6 +37,8 @@ if(reply=='y'):
     firstname = input('enter your first name:\t')
     account = input('enter your account name: \t')
     password = input('enter your password: \t')
+
+    password = a.encryption(password,key)
 else:
     sys.exit()
 with open(r'password.csv','a',newline='\n') as mypasswrite:
--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import csv,sys
+from crypt import Cryption
 
-account_name = 'github'
-user_name = 'Lscdvf'
+account_name = 'gmail'
+user_name = 'Rahul'
+key = 3
+a = Cryption()
 
 with open('password.csv') as mypass:
     csv_reader = csv.DictReader(mypass)
 
     for line in csv_reader:
         if (account_name.lower() == line['account'].lower()) and (user_name.lower() == line['firstname'].lower()):
-            print(f'password of {line["account"]} owned by {line["firstname"]}:{line["password"]}')
+            print(f'password of {line["account"]} owned by {line["firstname"]}:{a.decrytion(line["password"],key)}')
             sys.exit()
 
 with open(r'password.csv','a',newline='\n') as mypasswrite:
     fieldnames = ['firstname','account','password']
     writer = csv.DictWriter(mypasswrite,fieldnames=fieldnames)
-
-    writer.writerow({'firstname':'Rahul','account':'gmail','password':'rahmailg'})
+    
+    writer.writerow({'firstname':'Rahul','account':'gmail','password':a.encryption('rahmailg',key)})
 
